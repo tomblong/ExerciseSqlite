04426982667dfb5f50fcc138931ee20fbfb52b78
tomblong <haniffauzan200015@gmail.com>
2020-04-20 22:48:13 +0700

    InitialCommit still error

---
+++ app/src/main/java/com/aplikasi/exercisesqlite/MainActivity.java
@@ -0,0 +1,68 @@
+package com.aplikasi.exercisesqlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView listView;
+    DBHelper mydb;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), tambahdata.class);
+                startActivity(i);
+            }
+        });
+        mydb = new DBHelper(this);
+        ArrayList arrayList = mydb.getAllContacts();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView = (ListView) findViewById(R.id.listView1);
+        registerForContextMenu(listView);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                //TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(), tambahdata.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
+    }
+    public boolean onKeyDown(int keycode, KeyEvent event){
+        if (keycode == KeyEvent.KEYCODE_BACK){
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
+    }
+
+
+
+}
